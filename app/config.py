19732d21209bfb9c2ca01d1814ba01a2dda16231
MezhitovIsmail <113972330+MezhitovIsmail@users.noreply.github.com>
2025-01-11 14:04:23 +0300

    Add files via upload

---
+++ app/config.py
@@ -0,0 +1,11 @@
+import os
+
+PER_PAGE = 4
+SECRET_KEY = os.environ.get('SECRET_KEY') or '46b74dc7229cda8e0de864283dd32b07ed850f59f97aa1e209fedabf28c81283'
+
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://std_2438_game:12345678@std-mysql.ist.mospolytech.ru/std_2438_game'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_ECHO = True
+
+UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'media', 'images')
+
