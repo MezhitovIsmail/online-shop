d856cb0c6845b807a99323c7686cfe2a28e57406
MezhitovIsmail <113972330+MezhitovIsmail@users.noreply.github.com>
2025-01-16 17:20:05 +0300

    Update test_app.py

--- app/test_app.py
+++ app/test_app.py
@@ -2,10 +2,10 @@ def test_index():
     # Всегда считаем, что тест прошел
     assert True
 
-def test_index():
+def test_index2():
     # Всегда считаем, что тест прошел
     assert True
 
-def test_index():
+def test_index3():
     # Всегда считаем, что тест прошел
     assert True
